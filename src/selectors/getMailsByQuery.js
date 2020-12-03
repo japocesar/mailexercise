61b37e942bf95b58bcc2763a4403527a3db60441
japocesar <cesar.carrasco.s@hotmail.com>
2020-12-03 14:17:05 -0600

    remove useless js

--- src/selectors/getMailsByQuery.js
+++
@@ -1,8 +0,0 @@
-export const getMailsByQuery = ( query, mails ) => {
-    return mails.filter( mail => ( 
-        mail.body.toLowerCase().includes(query)
-        || mail.subject.toLowerCase().includes(query)
-        || mail.sender.toLowerCase().includes(query)
-        ) 
-    )
-}
